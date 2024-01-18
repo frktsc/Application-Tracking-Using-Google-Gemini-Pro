a071540ff8ff854252e193707819921a0503aa75
frktsc <ftasc40@gmail.com>
2024-01-18 11:01:46 -0500

    last changes

--- app.py
+++ app.py
@@ -50,6 +50,8 @@ submit1 = st.button("Tell Me About the Resume")
 
 submit2 = st.button("Percentage match")
 
+submit3=st.button("write a cover letter")
+
 input_prompt1 = """
  You are an experienced Technical Human Resource Manager,your task is to review the provided resume against the job description. 
   Please share your professional evaluation on whether the candidate's profile aligns with the role. 
@@ -60,6 +62,12 @@ input_prompt2 = """
 You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
 your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
 the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+"""
+
+input_prompt3 = """
+We will give you the requirements of the company and the resume of the person who wants to apply.You will review the resume and write a cover letter in accordance with the job description.
+If you do not see the job experience section in the CV, write cover letter as if you are someone who is willing to improve in that field, not as if you are experienced.
+Examine the uploaded CV carefully and do not write anything that is not there.
 """
 
 if submit1:
@@ -75,6 +83,15 @@ elif submit2:
     if uploaded_file is not None:
         pdf_content=input_pdf(uploaded_file)
         response=get_response(input_prompt2,pdf_content,input_text)
+        st.subheader("The Repsonse is")
+        st.write(response)
+    else:
+        st.write("Please uplaod the resume")
+        
+elif submit3:
+    if uploaded_file is not None:
+        pdf_content=input_pdf(uploaded_file)
+        response=get_response(input_prompt3,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
     else:
