6d468ffbe05a1a9fbad795cfa94bc26bdcbff616
frktsc <ftasc40@gmail.com>
2024-01-20 15:43:05 -0500

    model improved

--- app.py
+++ app.py
@@ -1,42 +1,27 @@
 from dotenv import load_dotenv
 
 load_dotenv()
-import base64
-import io
 import streamlit as st
 import os
-from PIL import Image 
-import pdf2image
+import PyPDF2 as pdf
 import google.generativeai as genai
 
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
 def get_response(input,pdf_content,prompt):
-    model=genai.GenerativeModel("gemini-pro-vision")
+    model=genai.GenerativeModel("gemini-pro")
     response=model.generate_content([input,pdf_content[0],prompt])
     return response.text
 
-def input_pdf(uploaded_file):
+def input_pdf_text(uploaded_file):
+    reader=pdf.PdfReader(uploaded_file)
+    text=" "
+    for page in range(len(reader.pages)):
+        page=reader.pages[page]
+        text+=str(page.extract_text())
+    return text
     
-    if uploaded_file is not None:
-        images=pdf2image.convert_from_bytes(uploaded_file.read())
-        first_page=images[0]
 
-        # Convert to bytes
-        img_byte_arr = io.BytesIO()
-        first_page.save(img_byte_arr, format='JPEG')
-        img_byte_arr = img_byte_arr.getvalue()
-
-        pdf_parts = [
-            {
-                "mime_type": "image/jpeg",
-                "data": base64.b64encode(img_byte_arr).decode()  # encode to base64
-            }
-        ]
-        return pdf_parts
-    else:
-        raise FileNotFoundError("No file uploaded")
-    
 st.set_page_config(page_title="ATS Resume EXpert")
 st.header("Resume Tracking System")
 input_text=st.text_area("Job Description: ",key="input")
@@ -59,9 +44,22 @@ input_prompt1 = """
 """
 
 input_prompt2 = """
-You are an skilled ATS (Applicant Tracking System) scanner with a deep understanding of data science and ATS functionality, 
-your task is to evaluate the resume against the provided job description. give me the percentage of match if the resume matches
-the job description. First the output should come as percentage and then keywords missing and last final thoughts.
+    Hey Act Like a skilled or very experience ATS(Application Tracking System)
+    with a deep understanding of tech field,software engineering,data science ,data analyst
+    and big data engineer. Your task is to evaluate the resume based on the given job description.
+    You must consider the job market is very competitive and you should provide 
+    best assistance for improving thr resumes.
+    Also, examine the resume carefully and do not consider anything on the resume as a missing match.
+    Assign the percentage Matching based 
+    on Job describtion and
+    the missing keywords with high accuracy
+    resume:{text}
+    description:{jd}
+
+    I want the response in one single string having the structure
+    {{"JD Match":"%",
+    "MissingKeywords:[]",
+    "Profile Summary":""}}
 """
 
 input_prompt3 = """
@@ -72,7 +70,7 @@ Examine the uploaded CV carefully and do not write anything that is not there.
 
 if submit1:
     if uploaded_file is not None:
-        pdf_content=input_pdf(uploaded_file)
+        pdf_content=input_pdf_text(uploaded_file)
         response=get_response(input_prompt1,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
@@ -81,7 +79,7 @@ if submit1:
 
 elif submit2:
     if uploaded_file is not None:
-        pdf_content=input_pdf(uploaded_file)
+        pdf_content=input_pdf_text(uploaded_file)
         response=get_response(input_prompt2,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
@@ -90,7 +88,7 @@ elif submit2:
         
 elif submit3:
     if uploaded_file is not None:
-        pdf_content=input_pdf(uploaded_file)
+        pdf_content=input_pdf_text(uploaded_file)
         response=get_response(input_prompt3,pdf_content,input_text)
         st.subheader("The Repsonse is")
         st.write(response)
